b78597057201a0cb5cfef98f0d8da8f924312315
Mikel Larreategi <mlarreategi@codesyntax.com>
2010-11-25 14:32:37 +0000

    new navigation for INavigationRoot enabled items

--- cs/bodysection/navigation.py
+++ cs/bodysection/navigation.py
@@ -1,14 +1,25 @@
 from zope.interface import Interface, implements
 from zope.component import queryMultiAdapter
 
-from Acquisition import aq_inner
+from Acquisition import aq_inner, aq_parent
 from Products.Five.browser import BrowserView
+
+from plone.memoize.view import memoize
+from zope.component import getMultiAdapter
+from plone.app.layout.navigation.interfaces import INavigationRoot
 
 
 class INavigationRootContent(Interface):
     def section_content_body_class():
-        pass
+        """ return a unique section class based on the id of the nearer child of a 
+        INavigationRoot item parent of the current context
+        """
 
+    def is_navigation_root_or_default_page():
+        """ return whether the current object is the navigation
+            root element or the default page of a navigation root
+            item. This is similar to @@ploneview/isPortalOrPortalDefaultPage method
+        """
 
 class NavigationRootContent(BrowserView):
     implements(INavigationRootContent)
@@ -24,3 +35,15 @@ class NavigationRootContent(BrowserView):
         else:
             return ''
 
+    @memoize
+    def is_navigation_root_or_default_page(self):
+        context = aq_inner(self.context)
+        context_state = getMultiAdapter(
+            (context, self.request), name=u'plone_context_state')
+        if context_state.is_default_page():
+            return INavigationRoot.providedBy(aq_parent(context))
+        else:
+            return INavigationRoot.providedBy(context)
+        
+        
+        
