651638e8728c80582a17dfec8cc6a2774ef23de1
Mikel Larreategi <mlarreategi@codesyntax.com>
2010-12-20 14:23:23 +0000

    small fixes

--- cs/bodysection/navigation.py
+++ cs/bodysection/navigation.py
@@ -1,24 +1,13 @@
 from zope.interface import Interface, implements
 from zope.component import queryMultiAdapter
-
-from Acquisition import aq_inner, aq_parent
+from Acquisition import aq_inner
 from Products.Five.browser import BrowserView
-
-from plone.memoize.view import memoize
-from zope.component import getMultiAdapter
-from plone.app.layout.navigation.interfaces import INavigationRoot
-
+from plone.app.layout.navigation.root import getNavigationRootObject
 
 class INavigationRootContent(Interface):
     def section_content_body_class():
         """ return a unique section class based on the id of the nearer child of a 
         INavigationRoot item parent of the current context
-        """
-
-    def is_navigation_root_or_default_page():
-        """ return whether the current object is the navigation
-            root element or the default page of a navigation root
-            item. This is similar to @@ploneview/isPortalOrPortalDefaultPage method
         """
 
 class NavigationRootContent(BrowserView):
@@ -27,23 +16,18 @@ class NavigationRootContent(BrowserView):
     def section_content_body_class(self):
         portal_state = queryMultiAdapter((self.context, self.request),
                                          name='plone_portal_state')
-        navigation_root_path = portal_state.navigation_root_path()
-        root_path_length = len(navigation_root_path)
-        path_name = '-'.join(self.context.getPhysicalPath())[root_path_length:]
-        if path_name != '-':
-            return 'content-section' + path_name
+
+        context = aq_inner(self.context)
+        portal = portal_state.portal
+        navigation_root = getNavigationRootObject(context, portal)
+        root_path_len = len(navigation_root.getPhysicalPath())
+        current_path = context.getPhysicalPath()
+        if len(current_path) >= root_path_len:
+            return 'content-section-' + current_path[root_path_len]
         else:
             return ''
 
-    @memoize
-    def is_navigation_root_or_default_page(self):
-        context = aq_inner(self.context)
-        context_state = getMultiAdapter(
-            (context, self.request), name=u'plone_context_state')
-        if context_state.is_default_page():
-            return INavigationRoot.providedBy(aq_parent(context))
-        else:
-            return INavigationRoot.providedBy(context)
+
         
         
         
